3186b645a5f076eabd3cfcbbfe28c2701b932788
Tim Arbaev <timbrk@gmail.com>
2015-07-29 20:37:22 +0300

    Свёрстано модальное окно загрузки проекта

---
+++ src/js/addproject.js
@@ -0,0 +1,17 @@
+var addModule = (function() {
+
+	var init = function () {
+		// инициализация функций
+		_setupListeners();
+	};
+
+	var _setupListeners = function () {
+		// прослушка событий
+	}
+
+	return {
+		init : init
+	};
+})();
+
+addModule.init();
