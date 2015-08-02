d3137664bf9760d8043349f2eb07e3f2e3dd7cde
Tim Arbaev <timbrk@gmail.com>
2015-08-02 16:34:04 +0300

    Переделан инпут аплоада файла, проверка на ввод данных

--- src/js/addproject.js
+++ src/js/addproject.js
@@ -1,18 +1,20 @@
+'use strict'
+
 var addModule = (function() {
 
+	// инициализация функций
 	var init = function () {
-		// инициализация функций
 		_setupListeners();
 	};
 
+	// прослушка событий
 	var _setupListeners = function () {
-		// прослушка событий
 		$('#add-new-project').on('click', _showPopup);
-		$('#form-add-project').on('submit', _addProject);
-
+		$('#form-add-project').on('submit', _submitProject);
 	};
 
-	var _showPopup = function(ev) {  // показать модальное окно добавления проекта
+	// показать модальное окно добавления проекта
+	var _showPopup = function(ev) {
 		ev.preventDefault();
 		$('#add-popup').bPopup({
 			modalClose: false,
@@ -21,61 +23,27 @@ var addModule = (function() {
 		});
 	};
 
-	var _addProject = function(ev) { // сабмит добавления проекта
-		console.log('submit project');
+	// сабмит добавления проекта
+	var _submitProject = function(ev) {
 		ev.preventDefault();
 
 		// объявляем нужные переменные
 		var form = $(this),
-			url = 'addproject.php';
-			serverGiveMeAnswer = _ajaxForm(form, url);
+			inps = form.find('input, textarea'),
+			tooltips = form.find('.label').next(), // тултип всегда идёт следом за label
+			url = 'addproject.php',
+			aaa;
 
-			serverGiveMeAnswer.done(function(answer) {
-				console.log("done!");
-				console.log(answer);
-				if (answer === 'OK') {
-					console.log(answer.text);
-					form.find('.success-msg').text(answer.text).show();
-				}else{
-					form.find('.msg > .msg-text').text(answer.text);
-					form.find('.err-msg').show();
-				}
-			});
+		inps.each(function() {
+			if($(this).val().length === 0) {
+				console.log(1);
 
-			serverGiveMeAnswer.always(function() {
-				console.log("always!");
-			});
-
-	};
-
-	// универсальная функция работы с формой
-	// @form - форма
-	// @url - путь/имяфайла к обработчику формы
-	//
-	// 1. проверить введённые данные
-	// 2. собрать данные из формы
-	// 3. отправить ответ с сервера
-	//
-	var _ajaxForm = function(form, url) {
-
-		data = form.serialize();
-
-		result = $.ajax({
-				url: url,
-				type: 'post',
-				dataType: "json",
-				data: data,
-			}).fail(function(answer) {
-				console.log("problem in php");
-					console.log(answer());
-
-				form.find('.msg > .msg-text').text(answer.text);
-				form.find('.err-msg').show();
-				// form.find('.err-msg').text('На сервере произошла ошибка').show();
-			});
-
-		return result;
-
+				$(this).addClass('err-input');
+				$(this).prev().removeClass('noshow');
+			} else {
+				return 1;
+			}
+		});
 
 	};
 
